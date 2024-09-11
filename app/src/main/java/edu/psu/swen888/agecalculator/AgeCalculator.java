b4b48b0f976e627ebafc245fbe6e46c6e428e6b2
Thenthprime <122050458+Thenthprime@users.noreply.github.com>
2024-09-11 15:33:18 -0500

    working buttons

--- app/src/main/java/edu/psu/swen888/agecalculator/AgeCalculator.java
+++ app/src/main/java/edu/psu/swen888/agecalculator/AgeCalculator.java
@@ -9,7 +9,8 @@ import android.widget.TextView;
 
 public class AgeCalculator extends AppCompatActivity  {
     private TextView mResultTextView;
-    private EditText mInputNumberEditText;
+    private EditText mInputDOBEditText;
+    private EditText mInputFirstEditText;
     private Button mCalculateAgeButton;
 
     @Override
@@ -19,7 +20,8 @@ public class AgeCalculator extends AppCompatActivity  {
         setContentView(R.layout.activity_age_calculator);
 
         mResultTextView = findViewById(R.id.result_text);
-        mInputNumberEditText = findViewById(R.id.number_field);
+        mInputDOBEditText = findViewById(R.id.dOB_field);
+        mInputFirstEditText = findViewById(R.id.firstName_field);
         mCalculateAgeButton = findViewById(R.id.check_button);
 
         mCalculateAgeButton.setOnClickListener(new View.OnClickListener(){
@@ -27,12 +29,13 @@ public class AgeCalculator extends AppCompatActivity  {
 
             public void onClick(View v){
                 //Get the number from the EditText
-                String numberString = mInputNumberEditText.getText().toString();
+                String numberString = mInputDOBEditText.getText().toString();
                 int number = Integer.parseInt(numberString);
+                String firstName = mInputFirstEditText.getText().toString();
 
                   int calculateAge = calculateAge(number);
 
-                mResultTextView.setText(calculateAge(number));
+                mResultTextView.setText(firstName + ", in case you forgot, you are " + calculateAge(number) + " years old!");
                 mResultTextView.setVisibility(View.VISIBLE);
             }
         });
